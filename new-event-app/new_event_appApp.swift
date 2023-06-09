782b91ec7577090ccb1629ce5553c5cdda6522de
bvechiato <beaaccounts@icloud.com>
2023-06-09 11:14:05 +0100

    Initial Commit

---
+++ new-event-app/new_event_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  new_event_appApp.swift
+//  new-event-app
+//
+//  Created by bea on 09/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct new_event_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
